8d7912b68964ec5397a3e43e2fda481567cae37c
Pratik Sutariya <pratik97233@gmail.com>
2024-03-08 23:34:01 +0530

    Latest

--- ca_frontend/src/components/auth/LoginForm.vue
+++ ca_frontend/src/components/auth/LoginForm.vue
@@ -6,27 +6,27 @@ const checkbox = ref(true);
 <template>
     <v-row class="d-flex mb-3">
         <v-col cols="12">
-            <v-label class="font-weight-bold mb-1">Username</v-label>
-            <v-text-field variant="outlined" hide-details color="primary"></v-text-field>
+            <v-label class="font-weight-bold mb-1">Email</v-label>
+            <v-text-field variant="outlined" autocomplete="false" hide-details color="primary"></v-text-field>
         </v-col>
         <v-col cols="12">
             <v-label class="font-weight-bold mb-1">Password</v-label>
-            <v-text-field variant="outlined" type="password"  hide-details color="primary"></v-text-field>
+            <v-text-field variant="outlined" type="password" autocomplete="false" hide-details color="primary"></v-text-field>
         </v-col>
         <v-col cols="12" class="pt-0">
             <div class="d-flex flex-wrap align-center ml-n2">
-                <v-checkbox v-model="checkbox"  color="primary" hide-details>
+                <!-- <v-checkbox v-model="checkbox"  color="primary" hide-details>
                     <template v-slot:label class="text-body-1">Remeber this Device</template>
-                </v-checkbox>
+                </v-checkbox> -->
                 <div class="ml-sm-auto">
-                    <RouterLink to="/"
-                        class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium">Forgot
-                        Password ?</RouterLink>
+                    <RouterLink to="/" class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium"
+                        >Forgot Password ?</RouterLink
+                    >
                 </div>
             </div>
         </v-col>
         <v-col cols="12" class="pt-0">
-            <v-btn to="/" color="primary" size="large" block   flat>Sign in</v-btn>
+            <v-btn to="/" color="primary" size="large" block flat>Sign in</v-btn>
         </v-col>
     </v-row>
 </template>
